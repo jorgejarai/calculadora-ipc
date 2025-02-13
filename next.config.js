82f4de9ec2fcc508fc06a53a9555d29b4ae4aefb
Jorge Jara <jorgejarainostroza@gmail.com>
2025-02-13 18:53:20 -0300

    build: Actualizar dependencias y configuración

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const withPWA = require('next-pwa');
-
 const isProd = process.env.NODE_ENV === 'production';
+
+const withPWA = require('next-pwa')({
+  disable: !isProd,
+  dest: 'public',
+});
 
 module.exports = withPWA({
   i18n: {
     locales: ['es-CL'],
     defaultLocale: 'es-CL',
-  },
-  pwa: {
-    disable: !isProd,
-    dest: 'public',
   },
 });
